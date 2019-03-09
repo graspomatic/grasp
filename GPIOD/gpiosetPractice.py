af1667ba620ef0db4e32e4294406ba4ec578fa11
graspomatic <graspomatic@gmail.com>
2019-03-09 11:30:44 -0500

    added timer

--- GPIOD/gpiosetPractice.py
+++ GPIOD/gpiosetPractice.py
@@ -2,23 +2,8 @@ import gpiod
 import sys
 import threading
 
-
-with gpiod.Chip("gpiochip2") as chip:
-    offsets = [0]
-    values = [1]
-
-    lines = chip.get_lines(offsets)
-    lines.request(consumer="gpioset", type=gpiod.LINE_REQ_DIR_OUT)
-    vals = lines.set_values(values)
-
-    print('set complete')
-    timer = threading.Timer(5.0, turnOff())
-    timer.start()
-
-
-
-
 def turnOff():
+    print("made it to turnoff")
     with gpiod.Chip("gpiochip2") as chip:
         offsets = [0]
         values = [0]
@@ -33,3 +18,20 @@ def turnOff():
         for val in vals:
             print(val, end=' ')
         print()
+
+
+with gpiod.Chip("gpiochip2") as chip:
+    offsets = [0]
+    values = [1]
+
+    lines = chip.get_lines(offsets)
+    lines.request(consumer="gpioset", type=gpiod.LINE_REQ_DIR_OUT)
+    vals = lines.set_values(values)
+
+    print('set complete')
+    timer = threading.Timer(5.0, turnOff)
+    timer.start()
+
+
+
+
