65395bdf53f12138dfa6bba0c9193f1ed4a1ea83
graspomatic <graspomatic@gmail.com>
2019-03-09 11:37:27 -0500

    reordered function

--- GPIOD/gpiosetPractice.py
+++ GPIOD/gpiosetPractice.py
@@ -2,6 +2,24 @@ import gpiod
 import sys
 import threading
 
+
+
+
+with gpiod.Chip("gpiochip2") as chip:
+    offsets = [0]
+    values = [1]
+
+    lines = chip.get_lines(offsets)
+    lines.request(consumer="gpioset", type=gpiod.LINE_REQ_DIR_OUT)
+    vals = lines.set_values(values)
+
+    print('set complete')
+
+timer = threading.Timer(5.0, turnOff)
+timer.start()
+
+
+
 def turnOff():
     print("made it to turnoff")
     with gpiod.Chip("gpiochip2") as chip:
@@ -19,21 +37,3 @@ def turnOff():
         for val in vals:
             print(val, end=' ')
         print()
-
-
-with gpiod.Chip("gpiochip2") as chip:
-    offsets = [0]
-    values = [1]
-
-    lines = chip.get_lines(offsets)
-    lines.request(consumer="gpioset", type=gpiod.LINE_REQ_DIR_OUT)
-    vals = lines.set_values(values)
-
-    print('set complete')
-    timer = threading.Timer(5.0, turnOff)
-
-timer.start()
-
-
-
-
