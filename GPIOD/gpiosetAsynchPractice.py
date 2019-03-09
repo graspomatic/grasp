29affa4794931129f815b70d6784bae0a943acae
graspomatic <graspomatic@gmail.com>
2019-03-09 13:01:18 -0500

    added asynch version as class

--- GPIOD/gpiosetAsynchPractice.py
+++ GPIOD/gpiosetAsynchPractice.py
@@ -2,32 +2,34 @@ import gpiod
 import sys
 import asyncio
 
-async def main():
+
+class MAGS(object):
+    async def disable(self):
+        with gpiod.Chip("gpiochip2") as chip:
+            offsets = [0]
+            values = [1]
+
+            lines = chip.get_lines(offsets)
+            lines.request(consumer="gpioset", type=gpiod.LINE_REQ_DIR_OUT)
+            vals = lines.set_values(values)
+
+            await asyncio.sleep(0.029)
+
+            offsets = [0]
+            values = [0]
+            #lines = chip.get_lines(offsets)
+            vals = lines.set_values(values)
+            vals = lines.get_values()
+
+            for val in vals:
+                print(val, end=' ')
+            print()
 
 
-    with gpiod.Chip("gpiochip2") as chip:
-        offsets = [0]
-        values = [1]
-
-        lines = chip.get_lines(offsets)
-        lines.request(consumer="gpioset", type=gpiod.LINE_REQ_DIR_OUT)
-        vals = lines.set_values(values)
-
-        await asyncio.sleep(0.029)
-
-        offsets = [0]
-        values = [0]
-        #lines = chip.get_lines(offsets)
-        vals = lines.set_values(values)
-        vals = lines.get_values()
-
-        for val in vals:
-            print(val, end=' ')
-        print()
-
-
-asyncio.run(main())
+    asyncio.run(disable())
 
 
 
 
+if __name__ == "__main__":
+    mags = MAGS()
