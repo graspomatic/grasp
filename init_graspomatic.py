e0112546f8cad5fb8f4cddaa87cf9b67a5402dad
graspomatic <graspomatic@gmail.com>
2019-03-14 16:13:31 -0400

    changed controller client

--- init_graspomatic.py
+++ init_graspomatic.py
@@ -8,3 +8,41 @@ dxl=Dynamixel2Control.D2C()
 import MagControl
 import asyncio
 mags=MagControl.MAGS()
+
+
+
+async def handle_request(reader, writer):
+    data = await reader.read(100)
+    message = data.decode()
+
+    try:
+        str, at = message.split('@')
+        loop = asyncio.get_event_loop()
+        loop.create_task(say(str, float(at)))
+    except:
+        print("Bad message format (should be string@time)")
+
+    print("Send: %r" % message)
+    writer.write(data)
+    await writer.drain()
+
+    print("Close the client socket")
+    writer.close()
+
+
+loop = asyncio.get_event_loop()
+coro = asyncio.start_server(handle_request, '127.0.0.1', 8888, loop=loop)
+server = loop.run_until_complete(coro)
+
+# Serve requests until Ctrl+C is pressed
+print('Serving on {}'.format(server.sockets[0].getsockname()))
+try:
+    loop.run_forever()
+except KeyboardInterrupt:
+    pass
+
+# Close the server
+server.close()
+loop.run_until_complete(server.wait_closed())
+loop.close()
+
