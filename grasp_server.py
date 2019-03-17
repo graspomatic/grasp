ef0f5f82f20687d77c39ad4cd67a9e74f35a49ff
graspomatic <graspomatic@gmail.com>
2019-03-17 15:04:56 -0400

    building wait for dxl

--- grasp_server.py
+++ grasp_server.py
@@ -116,7 +116,7 @@ async def present(arms='neither', hand=-1):
 
 async def wait_for_dxl():
     print('waiting for dynamixel motors to stop moving')
-    moving = [0, 0, 0, 0, 0, 0]
+    moving = [8, 8, 8, 8, 8, 8]
     while max(moving) > 0:
         print(moving)
         print(sum(moving))
