425fd8f284fc8b51265c19c6d5c565ff05f8a6f9
graspomatic <graspomatic@gmail.com>
2019-03-24 14:55:34 -0400

    adding redis comms

--- Dynamixel2Control/Dynamixel2Control.py
+++ Dynamixel2Control/Dynamixel2Control.py
@@ -25,12 +25,6 @@ class D2C(object):
         else:
             print("Failed to change the baud rate!")
 
-        # check connection to motors
-        if not self.sync_get_position():
-            print("Failed to connect to servos, 12v power missing?")
-        else:
-            print("Communicating with servos")
-
         # Set up groups for sync reads and writes
         self.groupMoving = dxlfx.GroupSyncRead(self.port_handler, self.packet_handler, dxlcx.ADDR_MOVING, 1)
         for i in range(0, len(dxlcx.IDs)):                          # for each arm
@@ -48,6 +42,12 @@ class D2C(object):
                 self.groupGetGoalPosition.addParam(dxlcx.IDs[i][ii])  # add this motor to list
 
         self.groupSetPosition = dxlfx.GroupSyncWrite(self.port_handler, self.packet_handler, dxlcx.ADDR_GOAL_POSITION, 4)
+
+        # check connection to motors
+        if not self.sync_get_position():
+            print("Failed to connect to servos, 12v power missing?")
+        else:
+            print("Communicating with servos")
 
 
 
