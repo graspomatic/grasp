416c39f8e64b1fb0371a4ffb5028decfc842fe3e
root <graspomatic@gmail.com>
2019-03-25 21:43:58 +0000

    added install.sh script

---
+++ install.sh
@@ -0,0 +1,6 @@
+SITEDIR=$(python3 -m site --user-site)
+mkdir -p "$SITEDIR"
+echo "$HOME/grasp/Dynamixel2Control" > "$SITEDIR/grasp.pth"
+echo "$HOME/grasp/AppliedMotionControl" >> "$SITEDIR/grasp.pth"
+echo "$HOME/grasp/GPIOD" >> "$SITEDIR/grasp.pth"
+echo "$HOME/grasp/controller" >> "$SITEDIR/grasp.pth"
