21122e199ae0abd6b9719dfdc372ca091213fcf2
graspomatic <graspomatic@gmail.com>
2019-04-04 14:47:21 -0400

    working on getting arms to put object back

--- grasp_server.py
+++ grasp_server.py
@@ -197,9 +197,9 @@ async def pick_and_place(hand=[-1], left_id=[-1], right_id=[-1], left_angle=[0],
         arms = 'neither'
 
     # if holding anything in left arm
-    # await return_object(0)
+    await return_object(0)
     # if holding anything in right arm
-    await return_object(1)
+    # await return_object(1)
 
     if arms == 'left':
         await retrieve(side=0, objid=left_id)
