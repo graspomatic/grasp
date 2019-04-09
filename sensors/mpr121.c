cef39dc27ebff710c2a61cb1efbce8e554e9b395
graspomatic <graspomatic@gmail.com>
2019-04-09 17:43:24 -0400

    working on mpr121 formatting output

--- sensors/mpr121.c
+++ sensors/mpr121.c
@@ -21,22 +21,22 @@ int main()
 
 
   // 45 512 49 512 46 512 50 512 38 812 51 512
+  // 50 512 44 512 41 512 52 512 53 512 58 512
 
 
-  int val1;
-  int val2;
+  int val;
 
   mpr121_context dev = mpr121_init(MPR121_I2C_BUS, MPR121_DEFAULT_I2C_ADDR);
   usleep(50000);
-  //mpr121_context dev2 = mpr121_init(MPR121_I2C_BUS, MPR121_DEFAULT_I2C_ADDR + 1);
+  mpr121_context dev2 = mpr121_init(MPR121_I2C_BUS, MPR121_DEFAULT_I2C_ADDR + 1);
   usleep(50000);
   
   if(mpr121_config_an3944(dev) != UPM_SUCCESS){
     printf("unable to configure device\n");
   }
-//  if(mpr121_config_an3944(dev2) != UPM_SUCCESS){
-//    printf("unable to configure device2\n");
-//  }
+  if(mpr121_config_an3944(dev2) != UPM_SUCCESS){
+    printf("unable to configure device2\n");
+  }
 
   for (i=0; i<n || print_output; i++){
     // read nchannels (8 bits in LB and 2 bits in high byte) all at once
@@ -47,33 +47,33 @@ int main()
         int j, m;
         printf("Left: ");
         for (j = 0, m = 0; j < channels_to_read; j++, m+=2) {
-            val1 = filtdata[m];
-            val2 = (filtdata[m+1] << 8);
-            printf("%i \t", val1);
-            printf("%i \t", val2);
+//            val1 = filtdata[m];
+//            val2 = (filtdata[m+1] << 8);
+//            printf("%i \t", val1);
+//            printf("%i \t", val2);
 
-         // val = filtdata[m] | (filtdata[m+1] << 8);
-          //printf("%i \t", filtdata[m] | (filtdata[m+1] << 8));
-          //printf("%i \t", left_baseline[j] - val);
+          val = filtdata[m] | (filtdata[m+1] << 8);
+//          printf("%i \t", filtdata[m] | (filtdata[m+1] << 8));
+          printf("%i \t", left_baseline[j] - val);
     	}
       }
     }
 
     usleep(50000);
 
-//    if (mpr121_read_bytes(dev2, MPR121_ELE0_FILTDATA_REG, filtdata, channels_to_read*2) != UPM_SUCCESS) {
-//      printf("Error while reading filtered data\n");
-//    } else {
-//      if (print_output) {
-//        int j, m;
-//        printf("Right: ");
-//        for (j = 0, m = 0; j < channels_to_read; j++, m+=2) {
-//          val1 = filtdata[m] | (filtdata[m+1] << 8);
-//
-//          printf("%i \t", right_baseline[j] - val1);
-//        }
-//      }
-//    }
+    if (mpr121_read_bytes(dev2, MPR121_ELE0_FILTDATA_REG, filtdata, channels_to_read*2) != UPM_SUCCESS) {
+      printf("Error while reading filtered data\n");
+    } else {
+      if (print_output) {
+        int j, m;
+        printf("Right: ");
+        for (j = 0, m = 0; j < channels_to_read; j++, m+=2) {
+          val = filtdata[m] | (filtdata[m+1] << 8);
+
+          printf("%i \t", right_baseline[j] - val);
+        }
+      }
+    }
 
    if (print_output) {
 	 printf("\n");
