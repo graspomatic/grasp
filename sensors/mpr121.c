8c1f589ff2fe6faa4b6f8aca4ee45a108dadb213
graspomatic <graspomatic@gmail.com>
2019-04-09 17:53:18 -0400

    working on mpr121 formatting output

--- sensors/mpr121.c
+++ sensors/mpr121.c
@@ -43,7 +43,7 @@ int main()
         for (j = 0, m = 0; j < channels_to_read; j++, m+=2) {
           usleep(6000);
           val = filtdata[m] | (filtdata[m+1] << 8);
-          printf("%0x \t", left_baseline[j] - val);
+          printf("%0x \t", val);
     	}
       }
     }
@@ -59,7 +59,7 @@ int main()
         for (j = 0, m = 0; j < channels_to_read; j++, m+=2) {
           usleep(6000);
           val = filtdata[m] | (filtdata[m+1] << 8);
-          printf("%0x \t", right_baseline[j] - val);
+          printf("%0x \t", val);
         }
       }
     }
