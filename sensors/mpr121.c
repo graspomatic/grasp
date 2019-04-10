25bd7eaf63817b258a6f24622c9d772f2db705e7
graspomatic <graspomatic@gmail.com>
2019-04-10 11:46:13 -0400

    working on mpr121 formatting output

--- sensors/mpr121.c
+++ sensors/mpr121.c
@@ -37,7 +37,7 @@ int main()
         }
     }
 
-    reply = redisCommand(context, "PUBLISH WebClient bar");
+    reply = redisCommand(redisContext, "PUBLISH WebClient bar");
 
 
   mpr121_context dev = mpr121_init(MPR121_I2C_BUS, MPR121_DEFAULT_I2C_ADDR);
