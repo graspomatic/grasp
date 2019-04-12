b65d8a991001c5596b3200760fdcdf427cabcf39
graspomatic <graspomatic@gmail.com>
2019-04-12 14:09:59 -0400

    working on setting up redis ports 6379 and 6380

--- startup.sh
+++ startup.sh
@@ -1,8 +1,5 @@
 ifdown eth1
 ifup eth1
-/etc/init.d/redis-server-6379 stop
-/etc/init.d/redis-server-6379 start
-/etc/init.d/redis-server-6380 stop
-/etc/init.d/redis-server-6380 start
-cd /usr/bin
-./webdis &
+/usr/bin/redis-server /etc/redis/redis_6379.conf
+/usr/bin/redis-server /etc/redis/redis_6380.conf
+/usr/bin/webdis &
