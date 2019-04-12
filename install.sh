c87d226397ba71eb06b23c11a957cccc68d78c07
graspomatic <graspomatic@gmail.com>
2019-04-12 12:40:47 -0400

    added webdis to startup

--- install.sh
+++ install.sh
@@ -9,4 +9,7 @@ cp startup.sh /etc/init.d/startup.sh
 chmod +x /etc/init.d/startup.sh
 update-rc.d startup.sh defaults
 
-cp redis.conf /etc/redis/redis.conf
+cp redis6379.conf /etc/redis/
+cp redis6380.conf /etc/redis/
+mkdir /var/lib/redis/6379
+mkdir /var/lib/redis/6380
