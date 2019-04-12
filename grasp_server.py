0e6af8e86713b8c19b08755d186fec9a5a99c1d8
graspomatic <graspomatic@gmail.com>
2019-04-12 18:06:18 -0400

    building out interactions with redis

--- grasp_server.py
+++ grasp_server.py
@@ -145,8 +145,8 @@ async def retrieve(side=-1, objid=0):
 
     last_update, connected = await asyncio.gather(fut1, fut2)
 
-    print(int.from_bytes(last_update, byteorder='little'))
-    print(int.from_bytes(connected, byteorder='little'))
+    print(int(last_update))
+    print(int(connected))
     print(int(time.time()))
 
 
