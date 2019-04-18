bfb6a9eeaf6898ad46a094d0efe5e1497e009aa5
graspomatic <graspomatic@gmail.com>
2019-04-18 17:02:12 -0400

    troubleshooting mpr121

--- sensors/mpr121.c
+++ sensors/mpr121.c
@@ -240,6 +240,14 @@ int main()
             left_touched[j] = ((cal_left[j] - left_current[j]) > touched_thresh);
         }
 
+        for (j = 0; j < 6; j++) {
+            if ((left_current[j] - left_baseline[j]) > 100) {
+                print_output = 1;
+            } else {
+                print_output = 0;
+            }
+        }
+
         if (print_output) {
             for (j = 0; j < 6; j++) {
                 printf("%d", left_current[j]);
