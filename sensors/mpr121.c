1650ded56dc62dfdbd2982a95ab671cf659aabcc
graspomatic <graspomatic@gmail.com>
2019-04-22 16:48:45 -0400

    implementing stop and start of mpr121 to avoid magnet state change

--- sensors/mpr121.c
+++ sensors/mpr121.c
@@ -135,8 +135,8 @@ int main()
   int channels_to_read = 6;
 //  int left_baseline[6] = {504, 512, 510, 556, 544, 553};
 //  int right_baseline[6] = {507, 499, 517, 515, 516, 547};
-  int left_baseline[6] = {518, 535, 521, 567, 555, 572};
-  int right_baseline[6] = {517, 513, 526, 510, 525, 550};
+  int left_baseline[6] = {507, 525, 514, 558, 546, 560};
+  int right_baseline[6] = {509, 501, 518, 510, 519, 543};
   int left_current[6] = {0, 0, 0, 0, 0, 0};
   int right_current[6] = {0, 0, 0, 0, 0, 0};
   int left_touched[6] = {0, 0, 0, 0, 0, 0};
