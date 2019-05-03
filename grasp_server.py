7919e5d32b939d4228c7ce4eccae3f9114c7149a
graspomatic <graspomatic@gmail.com>
2019-05-03 13:38:49 -0400

    optimizing time for pick_and_place routine

--- grasp_server.py
+++ grasp_server.py
@@ -45,8 +45,8 @@ async def return_object(side=-1, add=[0,0]):
     # find nearest empty spot on grid
 
     # move x-y motors to that empty spot
-    x.move_location(location=float(add[0]), accel=75, vel=5)
-    y.move_location(location=float(add[1]), accel=75, vel=5)
+    x.move_location(location=float(add[0]), accel=75, vel=20)
+    y.move_location(location=float(add[1]), accel=75, vel=20)
 
     # if x and y are finished moving, move arm to 'pick' position
     await loop.create_task(wait_for_dxl())
@@ -106,9 +106,9 @@ async def retrieve(side=-1, objid=0, add=[0,0]):
 
     # move x-y motors to location of object
     print('moving x to ' + str(add[0]))
-    x.move_location(location=float(add[0]), accel=75, vel=5)
+    x.move_location(location=float(add[0]), accel=75, vel=20)
     print('moving y to ' + str(add[1]))
-    y.move_location(location=float(add[1]), accel=75, vel=5)
+    y.move_location(location=float(add[1]), accel=75, vel=20)
 
     #stop reading from sensors
     # await redisfast.set('get_left', '0')
