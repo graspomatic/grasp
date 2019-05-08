466bdd1fec8e032e0ed6d6a015a060a6124a375a
graspomatic <graspomatic@gmail.com>
2019-05-08 13:06:56 -0400

    optimizing time for pick_and_place routine

--- grasp_server.py
+++ grasp_server.py
@@ -58,8 +58,8 @@ async def return_object(side=-1, add=[0,0]):
     elif side == 1:
         dxl.set_profile_accel(motor=21, accel=500)
 
-    await loop.create_task(wait_for_dxl(250))
-    await loop.create_task(wait_for_xy(xtarg=xtarg, ytarg=ytarg, distance_thresh=(100+xy_accel*10)))
+    await loop.create_task(wait_for_dxl(300))
+    await loop.create_task(wait_for_xy(xtarg=xtarg, ytarg=ytarg, distance_thresh=(100+xy_accel*15)))
     dxl.move_arm_to_pos(arm=side, pos='pick')
     await pub.publish_json('WebClient', {"leftarm": "prep_pick", "rightarm": "prep_pick"})
 
@@ -87,7 +87,7 @@ async def return_object(side=-1, add=[0,0]):
     # await redisfast.set('get_right', '1')
 
     # ensure that object was released (i2c not showing anything)
-    await loop.create_task(wait_for_dxl(180))
+    await loop.create_task(wait_for_dxl(220))
 
     if side == 0:
         await pub.publish_json('WebClient', {"leftarm": "prep_pick", "leftsensor": "0"})
@@ -126,7 +126,7 @@ async def retrieve(side=-1, objid=0, add=[0,0]):
     # await asyncio.sleep(0.01)
 
     # move specified arm to 'pick' position
-    await loop.create_task(wait_for_xy(xtarg=xtarg, ytarg=ytarg, distance_thresh=(100+xy_accel*10)))
+    await loop.create_task(wait_for_xy(xtarg=xtarg, ytarg=ytarg, distance_thresh=(100+xy_accel*15)))
     await loop.create_task(wait_for_dxl(200))
     dxl.move_arm_to_pos(arm=side, pos='pick')
     await pub.publish_json('WebClient', {"leftarm": "prep_pick", "rightarm": "prep_pick", "xpos": str(add[0]), "ypos": str(add[1])})
