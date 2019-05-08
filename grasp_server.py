2ea35dd5009534c3929b8d101e055f1be80b9561
graspomatic <graspomatic@gmail.com>
2019-05-08 15:18:02 -0400

    optimizing time for pick_and_place routine

--- grasp_server.py
+++ grasp_server.py
@@ -59,7 +59,7 @@ async def return_object(side=-1, add=[0,0]):
         dxl.set_profile_accel(motor=21, accel=500)
 
     await loop.create_task(wait_for_dxl(300))
-    await loop.create_task(wait_for_xy(xtarg=xtarg, ytarg=ytarg, distance_thresh=(100+xy_accel*20)))
+    await loop.create_task(wait_for_xy(xtarg=xtarg, ytarg=ytarg, distance_thresh=(100+xy_accel*30)))
     dxl.move_arm_to_pos(arm=side, pos='pick')
     await pub.publish_json('WebClient', {"leftarm": "prep_pick", "rightarm": "prep_pick"})
 
@@ -126,7 +126,7 @@ async def retrieve(side=-1, objid=0, add=[0,0]):
     # await asyncio.sleep(0.01)
 
     # move specified arm to 'pick' position
-    await loop.create_task(wait_for_xy(xtarg=xtarg, ytarg=ytarg, distance_thresh=(100+xy_accel*20)))
+    await loop.create_task(wait_for_xy(xtarg=xtarg, ytarg=ytarg, distance_thresh=(100+xy_accel*40)))
     await loop.create_task(wait_for_dxl(200))
     dxl.move_arm_to_pos(arm=side, pos='pick')
     await pub.publish_json('WebClient', {"leftarm": "prep_pick", "rightarm": "prep_pick", "xpos": str(add[0]), "ypos": str(add[1])})
@@ -179,7 +179,7 @@ async def retrieve(side=-1, objid=0, add=[0,0]):
 
     await pub.publish_json('WebClient', {"leftsensor": str(objid)})
     # ensure that object was picked up
-    await loop.create_task(wait_for_dxl(100))  #130 is probably too high
+    await loop.create_task(wait_for_dxl(120))  #130 is probably too high
 
 
 async def present(arms='neither', hand=-1, left_angle=0, right_angle=0):
