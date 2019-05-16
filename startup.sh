d6b90124eb10872bb8e7906744a80d198302f6f2
graspomatic <graspomatic@gmail.com>
2019-05-16 15:05:27 -0400

    getting nodejs running on minnowboard

--- startup.sh
+++ startup.sh
@@ -2,5 +2,4 @@ ifdown eth1
 ifup eth1
 /usr/bin/redis-server /etc/redis/redis_6379.conf
 /usr/bin/redis-server /etc/redis/redis_6380.conf
-cd /bin/webdis
-./webdis &
+webdis ./webdis_conf_files/webdis.prod.json
