53b0230a9db438ef7361ee11a060aa961946fe92
graspomatic <graspomatic@gmail.com>
2019-05-19 13:37:34 -0400

    getting nodejs running on minnowboard

--- node-web-server/server.js
+++ node-web-server/server.js
@@ -6,7 +6,9 @@ app.use(express.static('public'));
 //make way for some custom css, js and images
 app.use('/css', express.static(__dirname + '/public/css'));
 app.use('/js', express.static(__dirname + '/public/js'));
-app.use('../shapes/SVGs', express.static('../shapes/SVGs'));
+//app.use('../shapes/SVGs', express.static('../shapes/SVGs'));
+
+app.use('/public2', express.static('/home/root/grasp/shapes/SVGs')));
 
 var os = require('os');
 var ifaces = os.networkInterfaces();
