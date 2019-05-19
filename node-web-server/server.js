05403928bf7da98738f3a72e349166431f631c5c
graspomatic <graspomatic@gmail.com>
2019-05-19 13:08:44 -0400

    getting nodejs running on minnowboard

--- node-web-server/server.js
+++ node-web-server/server.js
@@ -1,13 +1,12 @@
+// set up express
 var express = require("express");
-
 var app = express();
-
 app.use(express.static('public'));
 
 //make way for some custom css, js and images
 app.use('/css', express.static(__dirname + '/public/css'));
 app.use('/js', express.static(__dirname + '/public/js'));
-app.use('/images', express.static(__dirname + '/public/images'));
+//app.use('/images', express.static(__dirname + '/public/images'));
 
 var os = require('os');
 var ifaces = os.networkInterfaces();
@@ -15,5 +14,5 @@ hostname = ifaces['eth0'][0]['address'];
 
 var server = app.listen(8081, hostname, function(){
     var port = server.address().port;
-    console.log("Server started at http://localhost:%s", port);
+    console.log('Server running at http://${hostname}:${port}/');
 });
