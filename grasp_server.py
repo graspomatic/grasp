134f2336a21ca0f6e98320e963340677ea827987
graspomatic <graspomatic@gmail.com>
2019-05-27 10:33:11 -0400

    setting up new full size panel

--- grasp_server.py
+++ grasp_server.py
@@ -515,7 +515,7 @@ async def find_bounds(axis = ['a'], direction = [-1]):
     if axis == 'x':
         await x.find_bound(direction, current=1.0)
     else:
-        await y.find_bound(direction, current=0.8)
+        await y.find_bound(direction, current=0.4)
 
 async def move_xy_distance_mm(axis = ['a'], distance = [0]):
     axis = str(axis[0])
