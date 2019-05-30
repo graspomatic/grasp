19f684c033caccddc3bcc4313781ce00e5e9a702
graspomatic <graspomatic@gmail.com>
2019-05-30 16:35:46 -0400

    implementing object database in redis

--- node-web-server/server.js
+++ node-web-server/server.js
@@ -14,7 +14,7 @@ let db = new sqlite3.Database('/home/root/grasp/shapes/objects2.db', sqlite3.OPE
 });
 
 db.serialize(() => {
-  db.each('SELECT objectID as id, blobName as blobName, SVG as svg FROM objectsTable', (err, row) => {
+  db.each('SELECT objectID as id, blobName as blobName, SVG as svg FROM objectsTable WHERE objectID = 2001', (err, row) => {
     if (err) {
       console.error(err.message);
     }
