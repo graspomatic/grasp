e90151e1f4a148b8c357b4a292c25c902d9aff6a
graspomatic <graspomatic@gmail.com>
2019-05-30 14:35:20 -0400

    implementing object database in redis

--- node-web-server/server.js
+++ node-web-server/server.js
@@ -6,7 +6,7 @@ app.use(express.static('public'));
 const sqlite3 = require('sqlite3').verbose();
 
 // open database in memory
-let db = new sqlite3.Database('/home/root/grasp/shapes/objects.db', sqlite3.OPEN_READONLY (err) => {
+let db = new sqlite3.Database('/home/root/grasp/shapes/objects.db', sqlite3.OPEN_READONLY, (err) => {
   if (err) {
     return console.error(err.message);
   }
