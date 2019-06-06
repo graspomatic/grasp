e111577b1d5b89ec621e9be5d5cf1a7724ca54bd
graspomatic <graspomatic@gmail.com>
2019-06-06 17:10:53 -0400

    adding zeromq support for webpage

--- web/subber.js
+++ web/subber.js
@@ -1,3 +1,6 @@
+var redis = require("redis");
+var pub = redis.createClient();
+
 var zmq = require('zeromq')
   , sock = zmq.socket('sub');
 
@@ -6,8 +9,7 @@ sock.subscribe('sensor:');
 console.log('Subscriber connected to port 5681');
 
 sock.on('message', function(topic, message) {
-    console.log('received a message related to:', topic, 'containing message:', message);
-    console.log(typeof topic)
-    console.log(topic.length)
+    //console.log('received a message related to:', topic, 'containing message:', message);
     console.log(topic.toString('ascii'))
+    pub.publish("WebClient", "rightSensor=10,10,10,10,10,10");
 });
