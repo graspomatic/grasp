6efec21f1517579d30fe16330dc0fa7e863cc4e8
graspomatic <graspomatic@gmail.com>
2019-06-06 15:55:15 -0400

    adding zeromq support for webpage

--- web/subber.js
+++ web/subber.js
@@ -1,9 +1,9 @@
 var zmq = require('zeromq')
   , sock = zmq.socket('sub');
 
-sock.connect('tcp://127.0.0.1:3000');
-sock.subscribe('kitty cats');
-console.log('Subscriber connected to port 3000');
+sock.connect('tcp://127.0.0.1:5681');
+sock.subscribe('sensor:');
+console.log('Subscriber connected to port 5681');
 
 sock.on('message', function(topic, message) {
   console.log('received a message related to:', topic, 'containing message:', message);
