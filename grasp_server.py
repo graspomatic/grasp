af1fc0a062263862ee1e7a7a8f5c243cff652255
graspomatic <graspomatic@gmail.com>
2019-06-09 15:48:01 -0400

    implementing socket.io for opening websockets between node and a browser

--- grasp_server.py
+++ grasp_server.py
@@ -634,9 +634,9 @@ async def find_bounds(axis = ['a'], direction = [-1]):
         await x.find_bound(direction, current=1.0)
     else:
         if direction == 1:
-            await y.find_bound(direction, current=0.1)
+            await y.find_bound(direction, current=0.15)
         else:
-            await y.find_bound(direction, current=1)
+            await y.find_bound(direction, current=0.9)
 async def move_xy_distance_mm(axis = ['a'], distance = [0]):
     axis = str(axis[0])
     distance = float(distance[0])
