552e5590460cd4110fe14cf01d69a8a7d1fdde58
graspomatic <graspomatic@gmail.com>
2019-06-09 12:30:40 -0400

    implementing socket.io for opening websockets between node and a browser

--- node-web-server/index.js
+++ node-web-server/index.js
@@ -1,18 +1,10 @@
 var app = require('express')();
-var http = require('http').Server(app);
-var io = require('socket.io')(http);
-var port = process.env.PORT || 3000;
+var http = require('http').createServer(app);
 
 app.get('/', function(req, res){
-  res.sendFile(__dirname + '/index.html');
+  res.send('<h1>Hello world</h1>');
 });
 
-io.on('connection', function(socket){
-  socket.on('chat message', function(msg){
-    io.emit('chat message', msg);
-  });
-});
-
-http.listen(port, function(){
-  console.log('listening on *:' + port);
+http.listen(3000, function(){
+  console.log('listening on *:3000');
 });
