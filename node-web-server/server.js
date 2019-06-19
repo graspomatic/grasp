68bd459203d57b0f9546a2761002ab7ba4368e27
graspomatic <graspomatic@gmail.com>
2019-06-19 17:49:59 -0400

    adding socket communication to touch sensor server (DSERV)

--- node-web-server/server.js
+++ node-web-server/server.js
@@ -30,7 +30,10 @@ io.on('connection', function(socket){
 sock.on('message', function(topic, message) {
     full = topic.toString('ascii');
     console.log(full);
-    io.emit('chat message', full);
+
+    mes = message.toString('ascii');
+    console.log(mes);
+//    io.emit('chat message', full);
 //    var ss = full.split(' ');
 //    var label = ss[0].split(':');
 //    var sensor = label[1];
