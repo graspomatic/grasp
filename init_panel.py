149c392dc38b3d14fea451555d7ed35d370266f4
graspomatic <graspomatic@gmail.com>
2019-07-09 12:07:16 -0400

    updating for psychophysics room

--- init_panel.py
+++ init_panel.py
@@ -29,9 +29,9 @@ DNU = np.array([[5, 3], [5, 4], [9, 3], [9, 4]])
 # br = np.array([4.5, 487])     # bottom right x,y position using left arm
 
 tl = np.array([266, 11])     # top left x,y position using left arm
-tr = np.array([6.25, 8.5])     # top right x,y position using left arm
-bl = np.array([265.5, 488.5])   # bottom left x,y position using left arm
-br = np.array([4.5, 487])     # bottom right x,y position using left arm
+tr = np.array([6, 8.5])     # top right x,y position using left arm
+bl = np.array([265, 488.5])   # bottom left x,y position using left arm
+br = np.array([4, 487])     # bottom right x,y position using left arm
 
 # tlr = np.array([325, 9])     # top left x,y position using right arm
 # trr = np.array([64, 7])     # top right x,y position using right arm
