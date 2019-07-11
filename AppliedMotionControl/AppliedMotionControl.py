256e47af5c8de376adfed03438e46e84b7a03d8a
graspomatic <graspomatic@gmail.com>
2019-07-11 12:20:17 -0400

    updating for psychophysics room

--- AppliedMotionControl/AppliedMotionControl.py
+++ AppliedMotionControl/AppliedMotionControl.py
@@ -71,8 +71,11 @@ class AMC(object):
         # self.send_command("EP")         # request position
         self.send_command("IP")  # request position
         position = self.read_udp_once()  # get response
-        if len(position) == 0:
+        while len(position) == 0:
             print('didnt receive a position from the motor! trying again...')
+            self.read_udp_all()  # clear buffer
+            time.sleep(0.003)
+            self.send_command("IP")  # request position
             time.sleep(0.003)
             position = self.read_udp_once()  # get response
         return int(position[3:len(position)])
