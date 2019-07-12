7ac112a6b0461106f39ac367f429b27a3005f451
graspomatic <graspomatic@gmail.com>
2019-07-12 15:55:03 -0400

    updating for psychophysics room

--- startup.sh
+++ startup.sh
@@ -1,8 +1,7 @@
-ifdown eth1
-ifup eth1
+ifdown eth0
+ifup eth0
 /usr/bin/redis-server /etc/redis/redis_6379.conf
 /usr/bin/redis-server /etc/redis/redis_6380.conf
-cd /usr/bin/webdis
 webdis /home/root/grasp/webdis_conf_files/webdis.json
 
 dserv -d
