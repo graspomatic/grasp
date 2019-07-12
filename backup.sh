fcd36bef0a10b594e671e49911ff8de15bd65ade
graspomatic <48136875+graspomatic@users.noreply.github.com>
2019-07-12 16:36:56 -0400

    Update backup.sh

--- backup.sh
+++ backup.sh
@@ -2,4 +2,4 @@ mount diddy.neuro.brown.edu:/mnt/SharedDrives/Labfiles /shared/lab
 
 cp /var/lib/redis/6380/appendonly6380.aof /shared/lab/stimuli/grasp
 
-mv -- "/shared/lab/stimuli/grasp" "$(mktemp --dry-run appendonly6380.aofXXXXXXX)"
+mv -- /shared/lab/stimuli/grasp "$(mktemp --dry-run appendonly6380.aofXXXXXXX)"
