3e640a0e989e1ba839483808803820bdc7b49f21
graspomatic <graspomatic@gmail.com>
2019-07-17 11:16:54 -0400

    updating for psychophysics room

--- init_panel.py
+++ init_panel.py
@@ -23,11 +23,6 @@ DNU = np.array([[5, 3], [5, 4], [9, 3], [9, 4]])
 # bl = np.array([266.5, 489.5])   # bottom left x,y position using left arm
 # br = np.array([5, 487.5])     # bottom right x,y position using left arm
 
-# tl = np.array([266.5, 11])     # top left x,y position using left arm
-# tr = np.array([6.25, 8.5])     # top right x,y position using left arm
-# bl = np.array([265.5, 488.5])   # bottom left x,y position using left arm
-# br = np.array([4.5, 487])     # bottom right x,y position using left arm
-
 tl = np.array([266.5, 10.7])     # top left x,y position using left arm
 tr = np.array([6.25, 9])     # top right x,y position using left arm
 bl = np.array([265.8, 488.7])   # bottom left x,y position using left arm
@@ -106,10 +101,11 @@ print(panel)
 
 r = redis.Redis(host='localhost', port=6380, db=0)
 
-
-p_old = np.array(json.loads(r.get('panel')))
+p_old = np.array(json.loads(r.get('panelsdf')))
 
 print(p_old)
+
+
 
 panelJSON = json.dumps(panel.tolist())
 #r.set('panel', panelJSON)
