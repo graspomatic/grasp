e6cc187cf9f25622e7ccdee63b5b6f6478938ef0
graspomatic <48136875+graspomatic@users.noreply.github.com>
2019-07-23 09:40:33 -0400

    Update index.js

--- node-web-server/index.js
+++ node-web-server/index.js
@@ -20,5 +20,6 @@ http.listen(3000, function(){
 io.on('connection', function(socket){
   socket.on('chat message', function(msg){
     console.log('message: ' + msg);
+    io.emit('chat message', { for: 'everyone' });
   });
 });
