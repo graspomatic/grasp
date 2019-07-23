a4c0ef169081678a18ebba13fcf8ca0b2a5c6bfa
graspomatic <graspomatic@gmail.com>
2019-07-23 15:55:22 -0400

    updating node to communicate with dserv

--- node-web-server/server.js
+++ node-web-server/server.js
@@ -3,22 +3,17 @@ var express = require('express');
 var app = express();
 app.use(express.static('public'));
 
-const sqlite3 = require('sqlite3').verbose();
-
 var http = require('http').createServer(app);
 var io = require('socket.io')(http);        // used to simplify websockets
 
 var path = require('path');
 var net = require('net');
-//var pattern = 'sensor:0:vals';
 
 global.atob = require("atob"); // only necessary if im using atob to convert the utf16 base64 to a string
 
 // Create a "server" to receive updates from dserv_send
 var dserv_rx = net.createServer(function (socket) {
-    // Identify this client
-    socket.name = socket.remoteAddress + ":" + socket.remotePort;
-//    console.log("server " + socket.remoteAddress + ":" + socket.remotePort + " started");
+    socket.name = socket.remoteAddress + ":" + socket.remotePort;   // Identify this client
 
     // Handle incoming messages from clients - this just prints but would likely dispatch
     socket.on('data', function (data) {
@@ -43,29 +38,13 @@ var dserv_rx = net.createServer(function (socket) {
 	        console.log(utf16encoded);
 
             console.log(utf16encoded.charCodeAt(0))
-
-
         }
-
-
-        //console.log(result.toString('utf8',0,Buffer.byteLength(result)-1));
-        //console.log(linesSeparated);
-        //console.log(linesSeparated.length);
-
-
-
-        //var utf8encoded = Buffer.from(result, 'base64').toString('utf8');
-
-
-
-        //io.emit('chat message', result.toString('utf8',0,Buffer.byteLength(result)-1));
     });
 }).listen(0);
 
 // When the server is ready, this callback is run
 //  It gets the address information, registers, and adds the match for the pattern
 dserv_rx.on('listening', function() {
-
     var client = new net.Socket();
     var host = '127.0.0.1';
     var port = 4620;
@@ -109,17 +88,6 @@ dserv_rx.on('listening', function() {
     });
 });
 
-//var zmq = require('zeromq')
-//  , sock = zmq.socket('sub');
-
-//sock.connect('ipc:///tmp/dserv-pub');
-//sock.subscribe('sensor:');
-//console.log('receiving "sensor:" messages from zeromq')
-
-
-//app.get('/', function(req, res){
-//  res.sendFile(__dirname + '/public/index.html');
-//});
 
 // socket.io
 io.on('connection', function(socket){
@@ -129,60 +97,10 @@ io.on('connection', function(socket){
   });
 });
 
-//sock.on('message', function(topic, message) {
-//    full = topic.toString('ascii');
-//    console.log(full);
-//
-//    mes = message.toString('ascii');
-//    console.log(mes);
-//    io.emit('chat message', full);
-//    var ss = full.split(' ');
-//    var label = ss[0].split(':');
-//    var sensor = label[1];
-//    console.log(label);
-//    console.log(ss[6].slice(1));
-//
-//    if (sensor == 0) {
-//        pub.publish("WebClient", "rightSensor=" +
-//        ss[6].slice(1) + "," + ss[7] + "," +
-//        ss[8] + "," + ss[9] + "," + ss[10] + "," +
-//        ss[11]);
-//    }
-
-
-//    console.log(typeof ss);
-//    console.log(ss);
-////    console.log(Object.keys(ss));
-//
-////
-//    console.log(ss[0]);
-//    console.log(ss[7])
-
-//    pub.publish("WebClient", "rightSensor=10,10,10,10,10,10");
-//});
-
 // this is used for socket.io communication
 http.listen(3000, function(){
   console.log('listening on *:3000');
 });
-
-// open database connection
-let db = new sqlite3.Database('/shared/lab/stimuli/grasp/objects2.db', sqlite3.OPEN_READONLY, (err) => {
-  if (err) {
-    return console.error(err.message);
-  }
-  console.log('Connected to the objects database.');
-});
-
-//example of retrieving stuff from sqlite
-//db.serialize(() => {
-//  db.each('SELECT objectID as objectID, blobName as blobName, SVG as SVG FROM objectsTable', (err, row) => {
-//    if (err) {
-//      console.error(err.message);
-//    }
-//    console.log(row.objectID + "\t" + row.blobName + "\t" + row.SVG);
-//  });
-//});
 
 app.use('/SVGs', express.static('/home/root/grasp/shapes/SVGs'));
 
@@ -196,37 +114,6 @@ var server = app.listen(8081, hostname, function(){
     var port = server.address().port;
     console.log('Server running at http://${hostname}:${port}/');
 });
-//
-//// modify this function to get whatever we need from the database
-//app.get('/dbgetformfills', function(req, res) {
-//
-//    // Get the list of categories the client is requesting and comma-separate them
-//    let cats=Object.keys(req.query);
-//    let catsString=cats.join(", ");
-//
-//    console.log(catsString);
-//
-//    // Connect to the DB
-//    let database = 'formFills.db';
-//    let formdb = connectDB(database);
-//
-//    console.log(database);
-//
-//    // Get the data from the database and send it to the client
-//    formdb.all("SELECT " + catsString + " FROM FormOptions",[], (err, rows) => {
-//        if (err) {
-//            throw err;
-//        }
-//        res.setHeader('Content-Type', 'application/json');
-//        res.send(JSON.stringify(rows));
-//    });
-//});
-//
-// close the sqlite database connection
-db.close((err) => {
-  if (err) {
-    return console.error(err.message);
-  }
-  console.log('Close the database connection.');
-});
+
+
 
