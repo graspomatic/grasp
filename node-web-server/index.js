15466aa92681e0648d37a514f98b8e6cc19ed5c8
graspomatic <graspomatic@gmail.com>
2019-07-23 09:26:07 -0400

    updating node to communicate with dserv

--- node-web-server/index.js
+++ node-web-server/index.js
@@ -1,15 +1,8 @@
 var app = require('express')();
-var http = require('http').Server(app);
-var io = require('socket.io')(http);
+var http = require('http').createServer(app);
 
 app.get('/', function(req, res){
-  res.sendFile(__dirname + '/public/index.html');
-});
-
-io.on('connection', function(socket){
-  socket.on('chat message', function(msg){
-    io.emit('chat message', msg);
-  });
+  res.send('<h1>Hello world</h1>');
 });
 
 http.listen(3000, function(){
