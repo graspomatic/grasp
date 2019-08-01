847e1935c20813f2d8b5a20f46bb42b8e1c99a5d
graspomatic <graspomatic@gmail.com>
2019-08-01 14:28:51 -0400

    fixing startup.sh

--- startup.sh
+++ startup.sh
@@ -2,10 +2,11 @@ ifdown eth0
 ifup eth0
 /usr/bin/redis-server /etc/redis/redis_6379.conf
 /usr/bin/redis-server /etc/redis/redis_6380.conf
-webdis /home/root/grasp/webdis_conf_files/webdis.json
 
-dserv -d
-dserv_tcp -d
-dserv_send -d
+/usr/bin/dserv -d
+/usr/bin/dserv_tcp -d
+/usr/bin/dserv_send -d
 
-mount diddy.neuro.brown.edu:/mnt/SharedDrives/Labfiles /shared/lab
+/bin/mount diddy.neuro.brown.edu:/mnt/SharedDrives/Labfiles /shared/lab
+
+webdis /home/root/grasp/webdis_conf_files/webdis.json &
