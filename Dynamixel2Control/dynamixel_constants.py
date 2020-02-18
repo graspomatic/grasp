15213c959bb43da1f06ea909be7e04172316ccee
graspomatic <graspomatic@gmail.com>
2020-02-18 16:07:51 -0500

    troubleshooting occasional failed pickup

--- Dynamixel2Control/dynamixel_constants.py
+++ Dynamixel2Control/dynamixel_constants.py
@@ -62,11 +62,9 @@ profile_accels_high = [[400, 0, 0],
 #motor 11, larger numbers means closer to panel
 #motor 21, smaller numbers means closer to panel
 
-# old, works fairly well
-#pick_pos = [[2065, 3264, 2024],
- #           [1515, 1040, 2010]]
 
-pick_pos = [[2075, 3264, 2024],
-            [1505, 1040, 2010]]
+pick_pos = [[2065, 3264, 2024],
+            [1515, 1040, 2010]]
+
 
 
