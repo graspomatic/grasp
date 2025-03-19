20b49fcbf06b35741f358f934a805699129dd601
graspomatic <48136875+graspomatic@users.noreply.github.com>
2025-03-19 14:22:39 -0400

    Update grasp_server.py

--- grasp_server.py
+++ grasp_server.py
@@ -537,7 +537,7 @@ async def pick_and_place(hand=[-1], left_id=[-1], right_id=[-1], left_angle=[180
     if dial_following and not pattern_following:
         await follow_dial_or_pattern(follow=["True"], mode=["dial"], offset=[left_angle])
     elif pattern_following and not dial_following:
-        await follow_dial_or_pattern(follow=["True"], mode=["pattern"], offset=[left_angle])
+        await follow_dial_or_pattern(follow=["True"], mode=["pattern"], offset=[left_angle-180])
     elif pattern_following and dial_following:
         print('cant do both dial and pattern following')
 
