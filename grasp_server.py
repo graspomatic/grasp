9839480540bfc3802c75dc3d05f27eca52d2c970
graspomatic <48136875+graspomatic@users.noreply.github.com>
2025-03-19 14:33:26 -0400

    Update grasp_server.py

--- grasp_server.py
+++ grasp_server.py
@@ -322,9 +322,9 @@ async def set_motor_to_dial_or_pattern():
                     send_to_dataserver(qnxsock, "grasp/left_angle", DservType.INT.value, int(target_angle % 360))
                 elif follow_settings["follow_mode"] == "pattern":
                     amplitude = 60     # will swing between +/- amplitude
-                    period = 10         # time in seconds of 1 rev
+                    period = 2         # time in seconds of 1 rev
                     elapsed = time.time() - follow_settings["start_time"]
-                    sine_value = amplitude * math.sin(2 * math.pi * elapsed) / period
+                    sine_value = amplitude * math.sin(2 * math.pi * elapsed / period)
                     target_angle = int(sine_value + follow_settings["offset"])
                     dxl.move_arm_to_pos(arm=follow_settings["target_arm"], pos='present', rotation=target_angle)
                     send_to_dataserver(qnxsock, "grasp/left_angle", DservType.INT.value, int(target_angle % 360))
