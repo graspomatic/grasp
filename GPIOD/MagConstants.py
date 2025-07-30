fefcabf15e780f8be5fdc68a9ba3367238497bb6
graspomatic <48136875+graspomatic@users.noreply.github.com>
2025-07-30 12:25:52 -0400

    Update MagConstants.py

--- GPIOD/MagConstants.py
+++ GPIOD/MagConstants.py
@@ -1,9 +1,9 @@
 #Pins
 CHIP = "gpiochip0"
-LEFT_HOLD = 65
-LEFT_RELEASE = 64
-RIGHT_HOLD = 62
-RIGHT_RELEASE = 63
+LEFT_HOLD = 13 # 65 on minnowboard (GPIO5) green
+LEFT_RELEASE = 19 # 64 on minnowboard (GPIO6) blue
+RIGHT_HOLD = 5 # 62 on minnowboard (GPIO3) red
+RIGHT_RELEASE = 6 # 63 on minnowboard (GPIO4) yellow
 
 
 RELEASE_DUR = 0.028 # time (s) to hold negative for degauss
