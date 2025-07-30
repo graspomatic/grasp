912dbe802d1fcbba790f1b18ea5e930f8202dcd9
graspomatic <48136875+graspomatic@users.noreply.github.com>
2025-07-30 13:42:25 -0400

    Update MagConstants.py

--- GPIOD/MagConstants.py
+++ GPIOD/MagConstants.py
@@ -1,5 +1,5 @@
 #Pins
-CHIP = "gpiochip0"
+CHIP = "/dev/gpiochip0"
 LEFT_HOLD = 13 # 65 on minnowboard (GPIO5) green
 LEFT_RELEASE = 19 # 64 on minnowboard (GPIO6) blue
 RIGHT_HOLD = 5 # 62 on minnowboard (GPIO3) red
